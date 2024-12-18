233085cf82e3757013a53ea15908ec5ef83aec1c
Clyne Wafula <clynewafula1@gmail.com>
2024-12-18 23:30:06 +0300

    html file for training

---
+++ indexc.js
@@ -0,0 +1,22 @@
+const n = document.getElementById("input");
+var replace = document.getElementById("disp");
+
+function button(input) {
+    n.value += input
+}
+
+function inputClear() {
+    n.value = "";
+}
+
+function calculate() {
+    n.value = eval(n.value)
+}
+
+function calculate() {
+    replace.innerHTML = "weeeee nakuona"
+}
+
+function calculate() {
+    replace.style.fontSize = '35px'
+}
